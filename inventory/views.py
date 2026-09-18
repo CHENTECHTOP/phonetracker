2ca69fa5c0c4799972936f637a9aba1dcbe71e5b
vladi <vladikakovichen@gmail.com>
2026-09-18 20:29:57 +0300

    логин та реєстрація

--- inventory/views.py
+++ inventory/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.views import View
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Device
 
 
-def home(request):
-    if request.method == 'POST':
+# Головна сторінка
+class HomePageView(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request):
+        devices = Device.objects.all().order_by('-id')
+        return render(request, 'phone/home.html', {'devices': devices})
+
+    def post(self, request):
         model_name = request.POST.get('model_name')
         imei = request.POST.get('imei_or_serial')
         status = request.POST.get('status')
@@ -20,19 +31,27 @@ def home(request):
             )
         return redirect('home')
 
-    all_devices = Device.objects.all().order_by('-id')
-    return render(request, 'phone/home.html', {'devices': all_devices})
+
+# Видалення пристрою
+class DeviceDeleteView(LoginRequiredMixin, View):
+    login_url = '/login/'
+
+    def get(self, request, pk):
+        device = get_object_or_404(Device, pk=pk)
+        device.delete()
+        return redirect('home')
 
 
-def delete_device(request, pk):
-    device = get_object_or_404(Device, pk=pk)
-    device.delete()
-    return redirect('home')
+# Редагування пристрою
+class DeviceEditView(LoginRequiredMixin, View):
+    login_url = '/login/'
 
+    def get(self, request, pk):
+        device = get_object_or_404(Device, pk=pk)
+        return render(request, 'phone/edit.html', {'device': device})
 
-def edit_device(request, pk):
-    device = get_object_or_404(Device, pk=pk)
-    if request.method == 'POST':
+    def post(self, request, pk):
+        device = get_object_or_404(Device, pk=pk)
         device.model_name = request.POST.get('model_name')
         device.imei_or_serial = request.POST.get('imei_or_serial')
         device.status = request.POST.get('status')
@@ -40,4 +59,18 @@ def edit_device(request, pk):
         device.purchase_price = request.POST.get('purchase_price')
         device.save()
         return redirect('home')
-    return render(request, 'phone/edit.html', {'device': device})
+
+
+# Реєстрація користувача
+class RegisterView(View):
+    def get(self, request):
+        form = UserCreationForm()
+        return render(request, 'phone/register.html', {'form': form})
+
+    def post(self, request):
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('home')
+        return render(request, 'phone/register.html', {'form': form})
